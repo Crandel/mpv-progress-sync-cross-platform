d3bdbdc501390175618c7e757c49900021ec7ac3
Vitalii Drevenchuk <4005032+Crandel@users.noreply.github.com>
2026-06-28 20:24:09 +0200

    Fix medium bugs

--- mpv-progress-sync/main.lua
+++ mpv-progress-sync/main.lua
@@ -5,73 +5,59 @@ duration = 0
 position = 0
 isPlaying = true
 encode = nil
+decode = nil
+md5 = nil
+currentFilename = ''
+
+-- Path configuration: single source of truth for all platform paths
+local positionFolders = {
+    Linux = os.getenv("HOME") .. '/.config/mpv/mpv-positions/',
+    OSX   = os.getenv("HOME") .. '/.config/mpv/mpv-positions/',
+    Windows = os.getenv("USERPROFILE") .. '\\scoop\\apps\\mpv\\current\\portable_config\\mpv-positions\\',
+    Android = '/storage/emulated/0/Android/media/is.xyz.mpv/mpv-positions/',
+}
+local scriptFolders = {
+    Linux = os.getenv("HOME") .. '/.config/mpv/scripts/mpv-progress-sync/lib/',
+    OSX   = os.getenv("HOME") .. '/.config/mpv/scripts/mpv-progress-sync/lib/',
+    Windows = os.getenv("USERPROFILE") .. '\\scoop\\apps\\mpv\\current\\portable_config\\scripts\\mpv-progress-sync\\lib\\',
+    Android = '/storage/emulated/0/Android/media/is.xyz.mpv/mpv-progress-sync/lib/',
+}
+
+-- Load decoder, encoder, and md5 once at startup
+function loadFile(path)
+    return assert(loadfile(path))()
+end
+
+local function initLibs()
+    local myos = getOS()
+    local scriptFolder = scriptFolders[myos]
+    if scriptFolder then
+        decode = loadFile(scriptFolder .. 'decoder.lua')()
+        encode = loadFile(scriptFolder .. 'encoder.lua')()
+        md5 = loadFile(scriptFolder .. 'md5.lua')
+    end
+end
+
+initLibs()
 
 
 -- This function is called when mpv loads a file
 mp.register_event("file-loaded", function()
-    -- Decode is used to un-marshall JSON data
-    decode = nil
     -- Call function to get users operating system
-    myos = getOS()
+    local myos = getOS()
     -- Call function to get current file
-    filename = getFilename(myos)
+    currentFilename = getFilename(myos)
     -- Strip filename of any escape characters
-    filename = string.gsub(filename, "[^%w%.%-_]", "_")
-    -- Get files duration from pv
+    currentFilename = string.gsub(currentFilename, "[^%w%.%-_]", "_")
+    -- Get files duration from mpv
     duration = mp.get_property_number("duration")
-    -- If the operating system is either Linux or Mac
-    if myos == 'Linux' or myos == 'OSX' then
-        -- Set folder to save position of files and the script folder location itself
-        linux_mac_position_folder = os.getenv("HOME") .. '/.config/mpv/mpv-positions/'
-        linux_script_folder       = os.getenv("HOME") .. '/.config/mpv/scripts/mpv-progress-sync/lib/'
-        -- Import decoder from from lunajson , call loadFile to open the file and then create a new decoder object
-        decoder_file              = linux_script_folder .. 'decoder.lua'
-        newdecoder                = loadfile(decoder_file)()
-        decode                    = newdecoder()
-        -- Import encoder from from lunajson , call loadFile to open the file and then create a new encoder object
-        encoder_file              = linux_script_folder .. 'encoder.lua'
-        newencoder                = loadfile(encoder_file)()
-        encode                    = newencoder()
-        -- Set global 'folder' where the position file is saved to the linux and mac location
-        folder                    = linux_mac_position_folder
-    end
-    if myos == "Android" then
-        -- Set folder to save position of files and the script folder location itself
-        android_position_folder = "/storage/emulated/0/Android/media/is.xyz.mpv/mpv-positions/"
-        android_script_folder =
-        '/storage/emulated/0/Android/media/is.xyz.mpv/mpv-progress-sync/lib/'
-        -- Import decoder from from lunajson , call loadFile to open the file and then create a new decoder object
-        decoder_file = android_script_folder .. 'decoder.lua'
-        newdecoder = loadfile(decoder_file)()
-        decode = newdecoder()
-        -- Import encoder from from lunajson , call loadFile to open the file and then create a new encoder object
-        encoder_file = android_script_folder .. 'encoder.lua'
-        newencoder = loadfile(encoder_file)()
-        encode = newencoder()
-        -- Set global 'folder' where the position file is saved to the android location
-        folder = android_position_folder
-    end
-    if myos == "Windows" then
-        -- Set folder to save position of files and the script folder location itself
-        windows_position_folder = os.getenv("USERPROFILE") ..
-            '\\scoop\\apps\\mpv\\current\\portable_config\\mpv-positions\\'
-        folder = windows_position_folder
-        windows_script_folder = os.getenv("USERPROFILE") ..
-            '\\scoop\\apps\\mpv\\current\\portable_config\\scripts\\mpv-progress-sync\\lib\\'
-        -- Import decoder from from lunajson , call loadFile to open the file and then create a new decoder object
-        decoder_file = windows_script_folder .. "decoder.lua"
-        newdecoder = loadfile(decoder_file)()
-        decode = newdecoder()
-        -- Import encoder from from lunajson , call loadFile to open the file and then create a new encoder object
-        encoder_file = windows_script_folder .. "encoder.lua"
-        newencoder = loadfile(encoder_file)()
-        encode = newencoder()
-    end
-    -- Set the filepath of the json file to be the combination of the folder and finelname with .json extension
-    filepath = folder .. filename .. ".json"
+    -- Set global 'folder' where the position file is saved
+    folder = positionFolders[myos] or ''
+    -- Set the filepath of the json file to be the combination of the folder and filename with .json extension
+    filepath = folder .. currentFilename .. ".json"
     -- Attempt to open the 'positionFile' using the filepath
     local positionFile, err = io.open(filepath, "r")
-    -- If the pssitionFile json is not present
+    -- If the positionFile json is not present
     if not positionFile then
         print("Could not open position file for reading:", err)
         return
@@ -109,27 +95,25 @@ timer = mp.add_periodic_timer(1, function()
 end)
 
 mp.register_event("shutdown", function()
-    -- Set isPlaying to false. This is because when existing mpv sets the 'time-pos' to nil so this stops the wrong position from being saved
+    -- Set isPlaying to false. This is because when exiting mpv sets the 'time-pos' to nil so this stops the wrong position from being saved
     isPlaying = false
 
-    -- If the position is not nill and it is greater than 2 seconds
+    -- If the position is not nil and it is greater than 2 seconds
     if position ~= nil and position > 2 then
         -- Sanitise the filename to remove escape characters
-        filename = string.gsub(filename, "[^%w%.%-_]", "_")
-        -- Get current OS
-        myos = getOS()
+        local filename = string.gsub(currentFilename, "[^%w%.%-_]", "_")
 
         -- Create the folder to save the positions of open files
-        if myos == "Windows" then
-            os.execute("mkdir " .. folder)
+        if getOS() == "Windows" then
+            os.execute('mkdir "' .. folder .. '"')
         else
-            os.execute("mkdir -p " .. folder)
+            os.execute('mkdir -p "' .. folder .. '"')
         end
         -- Create the filepath to save the position
         filepath = folder .. filename .. ".json"
-        -- Open tthe file
+        -- Open the file
 
-        print("Saving filepath: ",filepath)
+        print("Saving filepath: ", filepath)
         positionFile, err = io.open(filepath, "w")
         if not positionFile then
             print("Error opening file to write:", err)
@@ -141,7 +125,7 @@ mp.register_event("shutdown", function()
         if finalPosition <= 5 then
             position = 0
         end
-        -- Crete a table with the key 'loc'. The value is the opened files position
+        -- Create a table with the key 'loc'. The value is the opened files position
         local data = {
             loc = position
         }
@@ -155,46 +139,26 @@ mp.register_event("shutdown", function()
 end)
 
 
--- Helper function to load a file
-function loadFile(path)
-    return assert(loadfile(path))()
-end
-
-
 -- Help function to get the filename of a file. Parameter is the operating system of the user
 function getFilename(myos)
-
-    -- Open the md5 function from kikito, dependent on the operating system being used, This function is used to create a hash of the opened files filename
-    md5 = nil
-    if myos == 'Linux' or myos == 'OSX' then
-        md5 = loadFile(os.getenv("HOME") .. '/.config/mpv/scripts/mpv-progress-sync/lib/md5.lua')
-    end
-    if myos == "Android" then
-        md5 = loadFile(
-            '/storage/emulated/0/Android/media/is.xyz.mpv/mpv-progress-sync/lib/md5.lua')
-    end
-    if myos == "Windows" then
-        md5 = loadFile(os.getenv("USERPROFILE") ..
-            '\\scoop\\apps\\mpv\\current\\portable_config\\scripts\\mpv-progress-sync\\lib\\md5.lua')
-    end
 
     -- Check 'force-media-title' to determine if it is a youtube video
     local title = mp.get_property("force-media-title")
     -- If it is a YT video then use the YT video's title
     if title and #title > 0 then
         local fd = md5.sumhexa(title)
-        print("Hashed title file descriptor: ",fd)
+        print("Hashed title file descriptor: ", fd)
         -- Return the hashed title descriptor
         return fd
     end
     -- Get the file size and duration of the opened file in mpv
     local file_size = mp.get_property_number("file-size")
-    local duration = mp.get_property_number("duration")
+    local file_duration = mp.get_property_number("duration")
     -- Add the combination of the values together and convert to a string
-    local file_descriptor = tostring(file_size + duration)
+    local file_descriptor = tostring(file_size + file_duration)
     -- Use the md5 function to create a hash of the filename
     local fd = md5.sumhexa(file_descriptor)
-    print("Hashed file size and duration file descriptor: ",fd)
+    print("Hashed file size and duration file descriptor: ", fd)
     -- Return the hashed file size and duration descriptor
     return fd
 end
